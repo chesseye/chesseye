3329fa957c5b95d799632f2e436de6127ed55132
Jerome Simeon <simeon@us.ibm.com>
2016-05-07 15:58:33 -0400

    Add 'simulator' for one game

---
+++ controller/sample1.eye
@@ -0,0 +1,11 @@
+e2e4 d7d6
+d2d4 b8a6
+f1a6 b7a6
+g1f3 c8b7
+b1c3 e7e5
+d4d5 d8d7 
+O-O  O-O-O
+c1e3 f7f5
+e3a7 f5e4
+c3e4 b7d5
+d1d5 c7c6
---
+++ controller/simul.ml
@@ -0,0 +1,59 @@
+open Ochess
+
+exception WrongMove of string
+  
+let string_of_file file =
+  try
+    let inchan = open_in_bin file in
+    let len = in_channel_length inchan in
+    let buf = Buffer.create len in
+    Buffer.add_channel buf inchan len;
+    close_in inchan;
+    Buffer.contents buf
+  with
+    Sys_error err ->
+      Printf.eprintf
+        "Could not read the file %s, got error Sys_error %s\n@?"
+        file
+        err;
+      raise(Sys_error err)
+
+let positions_of_file f =
+  let fstring = string_of_file f in
+  let str_moves = Str.split (Str.regexp "[ \t\n]+") fstring in
+  let curpos = ref init_position in
+  let num = ref 0 in
+  let allpos = ref [init_position] in
+  let proc_one_move move_str =
+    let next_move =
+      match parse_move !curpos move_str with
+      | Some mv -> mv
+      | None -> raise (WrongMove ("In pos ["^move_str^"]"))
+    in
+    let next_pos = make_move !curpos next_move 0 in
+    begin
+      incr num;
+      allpos := next_pos :: !allpos;
+      curpos := next_pos
+    end
+  in
+  begin
+    List.iter proc_one_move str_moves;
+    print_endline ("Read " ^ (string_of_int !num) ^ " lines");
+    List.rev (!allpos)
+  end
+
+let bitmaps_of_positions positions =
+  List.map Util.string_of_position positions
+
+let print_positions f positions =
+  let bitmaps = bitmaps_of_positions positions in
+  let oc = open_out f in
+  List.iter (fun x -> output_string oc x; output_char oc '\n') bitmaps;
+  close_out oc
+
+let _ =
+  begin
+    let positions = positions_of_file "sample1.eye" in
+    print_positions "sample1.bmp" positions
+  end
