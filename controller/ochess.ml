1d27141a57321dc2e65e1a49b324bda0926fb1bb
Louis Mandel <github@louismandel.net>
2016-05-07 14:49:33 -0400

    Split types definitions.

--- controller/ochess.ml
+++ controller/ochess.ml
@@ -41,30 +41,11 @@ SOFTWARE.
 open Printf
 open Sys
 open Str
+open Types
 
 (* 
     Chess rules
 *)
-
-type color = Black | White
-
-type piece_type = King | Queen | Rook | Bishop | Knight | Pawn
-
-type piece = piece_type * color
-
-type field = Piece of piece | Empty
-
-type can_castle = bool * bool
-
-type position = { ar: field array array; 
-                  turn: color; 
-                  cas_w : can_castle; cas_b : can_castle; 
-                  en_passant : int option; 
-                  prev : position option; 
-                  irr_change : int; (* number of moves since capture or pawn move *)
-                  king_w : (int*int); king_b : (int * int); (* king positions are required often *)
-                  number : int; (* position number, initial position is 0 *)
-                  eval : int} (* essentially, memoized value of eval for this position *)
 
 let identical_positions x y = 
    x.eval = y.eval && (* if evals differ, positions are different *)
--- controller/ochess.mli
+++ controller/ochess.mli
@@ -1,21 +1,5 @@
-type color = Black | White
-type piece_type = King | Queen | Rook | Bishop | Knight | Pawn
-type piece = piece_type * color
-type field = Piece of piece | Empty
-type can_castle = bool * bool
-type position = {
-  ar : field array array;
-  turn : color;
-  cas_w : can_castle;
-  cas_b : can_castle;
-  en_passant : int option;
-  prev : position option;
-  irr_change : int;
-  king_w : int * int;
-  king_b : int * int;
-  number : int;
-  eval : int;
-}
+open Types
+
 val identical_positions : position -> position -> bool
 val draw_by_repetition_aux : position -> position -> int -> bool
 val draw_by_repetition : position -> bool
