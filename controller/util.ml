b2591e7d97c5d241099312e4f8e281af036d45df
Louis Mandel <github@louismandel.net>
2016-05-07 16:01:24 -0400

    Build a mask from a string.

--- controller/util.ml
+++ controller/util.ml
@@ -22,3 +22,33 @@ let string_of_position p =
   done;
   !acc
 
+let mask_of_string s : mask =
+  let board = Array.make_matrix 8 8 None in
+  let cpt = ref 0 in
+  for i = 0 to 7 do
+    for j = 0 to 7 do
+      let f =
+        begin match String.get s !cpt with
+        | '0' -> None
+        | '1' -> Some White
+        | _ -> failwith "mask_of_string: illegal"
+        end
+      in
+      board.(i).(j) <- f;
+      incr cpt
+    done
+  done;
+  for i = 0 to 7 do
+    for j = 0 to 7 do
+      let f =
+        begin match String.get s !cpt with
+        | '0' -> None
+        | '1' -> Some Black
+        | _ -> failwith "mask_of_string: illegal"
+        end
+      in
+      board.(i).(j) <- f;
+      incr cpt
+    done
+  done;
+  board
