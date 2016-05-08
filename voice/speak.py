3408365c5f3a6d274c0f41989de2cec948566cdf
Philippe Suter <philippe.suter@gmail.com>
2016-05-08 00:29:08 -0400

    producing SAN moves from FEN+UCI

--- voice/speak.py
+++ voice/speak.py
@@ -1,8 +1,17 @@
+import chess
 import sys
 import subprocess
 
+def say(text):
+    subprocess.call([ "say", text ])
+
+def say_fen_move(fen_str, uci_str):
+    board = chess.Board(fen_str)
+    move = board.parse_uci(uci_str)
+    say(board.san(move))
+
 def say_move(move_str):
-    subprocess.call([ "say", move_str ])
+    say(move_str)
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -13,6 +22,8 @@ if __name__ == "__main__":
 
     if verb == "MOVE":
         say_move(sys.argv[2])
+    elif verb == "FENM":
+        say_fen_move(sys.argv[2], sys.argv[3])
     else:
         sys.stderr.write("Unrecognized command: '%s'.\n" % verb)
         sys.exit(1)
