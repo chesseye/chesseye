2ea815ecade60b399641450454ad6493f60536ad
Louis Mandel <github@louismandel.net>
2016-05-07 23:14:32 -0400

    Add a parameter to ./slowdown to choose the delay.

--- controller/slowdown.ml
+++ controller/slowdown.ml
@@ -1,8 +1,15 @@
 let () =
+  let d =
+    if Array.length Sys.argv = 3 then
+      try float_of_string Sys.argv.(2)
+      with _ -> 0.01
+    else
+      0.01
+  in
   try
     while true do
       let line = input_line stdin in
-      Thread.delay 0.01;
+      Thread.delay d;
       print_endline line
     done
   with
