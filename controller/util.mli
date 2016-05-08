3dd0805ea4489c68ad9e579cb0fb76ba2f519b0a
Louis Mandel <github@louismandel.net>
2016-05-07 20:30:21 -0400

    Controller reads input.

---
+++ controller/util.mli
@@ -0,0 +1,18 @@
+open Types
+
+val warning : string -> unit
+val string_of_field : color -> field -> string
+val string_of_position : position -> string
+val mask_of_string : string -> mask
+val diff_mask :
+  mask ->
+  mask -> ((int * int) * color option * color option) list
+val consistent_line_and_color :
+  int * int * int * int ->
+  color * color * color * color -> bool
+val chose_castle_side : int * int * int * int -> dmove
+val dmove_of_masks : mask -> mask -> dmove
+val detect_promotion :
+  position -> int * int * int * int -> position
+val make_dmove : position -> dmove -> position
+val mask_of_position : position -> mask
