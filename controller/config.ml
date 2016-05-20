d6b08cf9d561d38753fa0ad3f6e22da6b84851a2
Louis Mandel <github@louismandel.net>
2016-05-20 18:44:31 -0400

    Add -sync-input option.

--- controller/config.ml
+++ controller/config.ml
@@ -1,10 +1,12 @@
 open Types
 
 let kibb = ref 3
+let sync_input = ref false
 
 let () =
   Arg.parse
-    [ "-kibbitz", Arg.Set_int kibb, "<n> where 0: no, 1: white, 2: black, 3: both" ]
+    [ "-kibbitz", Arg.Set_int kibb, "<n> where 0: no, 1: white, 2: black, 3: both";
+      "-sync-input", Arg.Set sync_input, "read the input in blocking mode"]
     (fun _ -> ())
     (Sys.argv.(0)^" [-kibbitz n]")
 
@@ -15,3 +17,5 @@ let advices =
   | 2 -> [Black]
   | 3 -> [White; Black]
   | _ -> [White; Black]
+
+let async_input = not !sync_input
--- controller/config.mli
+++ controller/config.mli
@@ -1,2 +1,3 @@
 open Types
 val advices : color list
+val async_input : bool
