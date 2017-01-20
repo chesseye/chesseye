d4d728ec29d99f0ac20f147f2bac272612ce9a5b
Louis Mandel <github@louismandel.net>
2017-01-20 16:40:09 -0500

    Handling options.

--- controller/config.ml
+++ controller/config.ml
@@ -1,21 +1,18 @@
 open Types
 
-let kibb = ref 3
+let advices = ref [White; Black]
+let set_advices kibb =
+  advices :=
+    begin match kibb with
+    | 0 -> []
+    | 1 -> [White]
+    | 2 -> [Black]
+    | 3 -> [White; Black]
+    | _ -> [White; Black]
+    end
+
 let sync_input = ref false
 
-let () =
-  Arg.parse
-    [ "-kibbitz", Arg.Set_int kibb, "<n> where 0: no, 1: white, 2: black, 3: both";
-      "-sync-input", Arg.Set sync_input, "read the input in blocking mode"]
-    (fun _ -> ())
-    (Sys.argv.(0)^" [-kibbitz n]")
-
-let advices =
-  match !kibb with
-  | 0 -> []
-  | 1 -> [White]
-  | 2 -> [Black]
-  | 3 -> [White; Black]
-  | _ -> [White; Black]
-
-let async_input = not !sync_input
+let options =
+  [ "-kibbitz", Arg.Int set_advices, "<n> where 0: no, 1: white, 2: black, 3: both";
+    "-sync-input", Arg.Set sync_input, " read the input in blocking mode"]
--- controller/config.mli
+++ controller/config.mli
@@ -1,3 +1,4 @@
 open Types
-val advices : color list
-val async_input : bool
+val advices : color list ref
+val sync_input : bool ref
+val options : (Arg.key * Arg.spec * Arg.doc) list
